d0535d85ead7b467b4959aac3208e4eb16b18e88
Suman Chettri <130631412+SumanChettri@users.noreply.github.com>
2025-03-06 12:58:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/my-vite-app/',
+base: '/My_Portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
